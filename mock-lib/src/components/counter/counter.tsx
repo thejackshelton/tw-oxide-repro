a79565d28ec2835eddb0806d6b2adc6a7bd55de3
thejackshelton <me@jackshelton.com>
2025-03-24 19:13:39 -0500

    add tw

--- mock-lib/src/components/counter/counter.tsx
+++ mock-lib/src/components/counter/counter.tsx
@@ -1,14 +1,14 @@
 import { component$, useSignal } from "@builder.io/qwik";
 
 export const Counter = component$(() => {
-  const count = useSignal(0);
+	const count = useSignal(0);
 
-  return (
-    <div>
-      <p>Count: {count.value}</p>
-      <p>
-        <button onClick$={() => count.value++}>Increment</button>
-      </p>
-    </div>
-  );
+	return (
+		<div class="bg-red-500">
+			<p>Count: {count.value}</p>
+			<p>
+				<button onClick$={() => count.value++}>Increment</button>
+			</p>
+		</div>
+	);
 });
